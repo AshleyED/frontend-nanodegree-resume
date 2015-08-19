55144bb023339816cdaea72e56e897d241750c38
Ashley Davison <ashley.e.davison@gmail.com>
2015-08-19 18:44:50 -0400

    Create header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+/* $("#main").append(["Ashley Davison"]);
+
+var awesomeThoughts = "I am Ashley and I am AWESOME!";
+
+console.log (awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace ("AWESOME!", "FUN!");
+
+$("#main").append(funThoughts); */
+
+var name = "Ashley Davison";
+var role = "Front-End Developer";
+
+var formattedName = HTMLheaderName.replace ("%data%" , name);
+$("#header").append(formattedName);
+console.log(formattedName);
+
+var formattedRole = HTMLheaderRole.replace ("%data%", role);
+$("#header").append(formattedRole);
+console.log(formattedRole);
